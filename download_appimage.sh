3bb4e990577348c47219c8e2be6d1f2234db1be2
Logan Lawrence <loganlawrence250@gmail.com>
2023-11-02 13:33:12 -0700

    master: typo in download_appimage.sh

--- download_appimage.sh
+++ download_appimage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 nvim="$HOME/.local/bin/nvim"
-nvimurl="https://github.com/neovim/neovim/releases/downoad/v0.9.0/nvim.appimage"
+nvimurl="https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage"
 mkdir -p "$(dirname "$nvim")"
 curl -fL "$nvimurl" -o "$nvim" -z "$nvim"
 chmod u+x "$nvim"
