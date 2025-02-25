de2d5b1c8738e30cdc37d964ad66c93872d08590
Logan Lawrence <lclawrence@umass.edu>
2025-02-25 00:54:18 +0000

    main: search regex in new tab

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<space>ff', builtin.find_files, {})
 vim.keymap.set('n', '<space>gf', builtin.git_files, {})
 vim.keymap.set('n', '<space>ps', function()
+	vim.cmd("tabnew") 
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
